611dceed5975ccb290b416a864f21d9bce734883
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-04 12:27:49 -0800

    added lsh_split_line()

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 
 #define LSH_RL_BUFSIZE 1024
 
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
 int main(int argc, char **argv)
 {
    // load config files, if any
@@ -76,4 +79,40 @@ char *lsh_read_line(void)
          }
       }
    }
+}
+
+char **lsh_split_line(char *line)
+{
+   int bufsize = LSH_TOK_BUFSIZE, position = 0;
+   char **tokens = malloc(bufsize * sizeof(char *));
+   char *token;
+
+   if (!tokens)
+   {
+      fprintf(stderr, "lsh: allocation error\n");
+      exit(EXIT_FAILURE);
+   }
+
+   token = strtok(line, LSH_TOK_DELIM);
+   while (token != NULL)
+   {
+      tokens[position] = token;
+      position++;
+
+      if (position >= bufsize)
+      {
+         bufsize += LSH_TOK_BUFSIZE;
+         tokens = realloc(tokens, bufsize * sizeof(char *));
+         if (!tokens)
+         {
+            fprintf(stderr, "lsh: allocation error\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+
+      token = strtok(NULL, LSH_TOK_DELIM);
+   }
+
+   tokens[position] = NULL;
+   return tokens;
 }
