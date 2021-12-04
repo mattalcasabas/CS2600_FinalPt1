40ca0e861969098ed0598c70130e692bf60e6558
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-04 08:53:32 -0800

    added lsh_read_line()

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define LSH_RL_BUFSIZE 1024
+
 int main(int argc, char **argv)
 {
    // load config files, if any
@@ -30,4 +32,48 @@ void lsh_loop()
       free(line);
       free(args);
    } while (status);
+}
+
+char *lsh_read_line(void)
+{
+   int bufsize = LSH_RL_BUFSIZE;
+   int position = 0;
+   char *buffer = malloc(sizeof(char) * bufsize);
+   int c;
+
+   if (!buffer)
+   {
+      fprintf(stderr, "lsh: allocation error\n");
+      exit(EXIT_FAILURE);
+   }
+
+   while (1)
+   {
+      // read character
+      c = getchar();
+
+      // if we hit EOF, replace it with null character and return
+      if (c == EOF || c == '\n')
+      {
+         buffer[position] = '\0';
+         return buffer;
+      }
+      else
+      {
+         buffer[position] = c;
+      }
+      position++;
+
+      // if we exceed the buffer, reallocate
+      if (position >= bufsize)
+      {
+         bufsize += LSH_RL_BUFSIZE;
+         buffer = realloc(buffer, bufsize);
+         if (!buffer)
+         {
+            fprintf(stderr, "lsh: allocation error\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+   }
 }
