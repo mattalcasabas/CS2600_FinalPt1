3d609c63adba69429c51ee56a92aaf90e1ad4828
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-04 12:58:10 -0800

    added lsh_execute()

--- main.c
+++ main.c
@@ -200,6 +200,27 @@ int lsh_launch(char **args)
    return 1;
 }
 
+int lsh_execute(char **args)
+{
+   int i;
+
+   if (args[0] == NULL)
+   {
+      // no commands were entered
+      return 1;
+   }
+
+   for (i = 0; i < lsh_num_builtins(); i++)
+   {
+      if (strcmp(args[0], builtin_str[i]) == 0)
+      {
+         return (*builtin_func[i])(args);
+      }
+   }
+
+   return lsh_launch(args);
+}
+
 int main(int argc, char **argv)
 {
    // load config files, if any
