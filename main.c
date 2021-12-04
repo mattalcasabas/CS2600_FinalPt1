f804b6ea1503c97fc25a30be3e7a337a803b3a4e
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-04 12:34:39 -0800

    added lsh_launch()

--- main.c
+++ main.c
@@ -116,3 +116,34 @@ char **lsh_split_line(char *line)
    tokens[position] = NULL;
    return tokens;
 }
+
+int lsh_launch(char **args)
+{
+   pid_t pid, wpid;
+   int status;
+
+   pid = fork();
+   if (pid == 0)
+   {
+      // child process
+      if (execvp(args[0], args) == -1)
+      {
+         perror("lsh");
+      }
+      exit(EXIT_FAILURE);
+   }
+   else if (pid < 0)
+   {
+      // error forking
+      perror("lsh");
+   }
+   else
+   {
+      do
+      {
+         wpid = waitpid(pid, &status, WUNTRACED);
+      } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+   }
+
+   return 1;
+}
